a2385f437758bac923cfdf5a4c932f2bd59ee83a
Klaudiaamarzec <klaudia06072002@onet.pl>
2024-04-13 18:57:49 +0200

    Beginning of frontend

---
+++ Frontend/script.js
@@ -0,0 +1,97 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var loginButton = document.getElementById("loginButton");
+
+    loginButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony loginPage.html
+        window.location.href = "loginPage.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var logoutButton = document.getElementById("logout");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    logoutButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "landingPage.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var homeButton = document.getElementById("homeButton");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    homeButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "studentHome.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var searchButton = document.getElementById("searchButton");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    searchButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "freeGroups.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupButton = document.getElementById("groupButton");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "yourGroup.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var enrollmentButton = document.getElementById("enrollmentButton");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    enrollmentButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "enrollment.html";
+    });
+});
+
+/* ADMIN VIEW*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var adminButton = document.getElementById("homeAdmin");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    adminButton.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "adminHome.html";
+    });
+});
+
+const login=() => {
+
+    const inputLogin = document.getElementById("login");
+    const inputPassword = document.getElementById("password");
+
+    let valueLogin = inputLogin.value;
+    let valuePassword = inputLogin.value;
+
+    console.log("Login: ", valueLogin);
+    console.log("Password: ", valuePassword);
+
+    if(valueLogin === "" || valuePassword ==="")
+    {
+        const message = document.getElementById("errorLogin");
+        message.style.display = "flex";
+    }
+}
