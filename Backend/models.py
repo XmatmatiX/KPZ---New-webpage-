e24afa1845dbe5d700f4409ec222046fe3ec5d4c
bParz156 <basia15p@gmail.com>
2024-04-18 16:07:03 +0200

    pliki

--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,68 @@
-from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey, UniqueConstraint, Sequence
+from sqlalchemy import  Column, Integer, String, Boolean, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from database import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Project(Base):
+    __tablename__ = 'Project'
+    ProjectID = Column(Integer, primary_key=True)
+    CompanyName = Column(String(50), nullable=False)
+    ProjectTitle = Column(String(255), nullable=False)
+    Email = Column(String(50), nullable=False)
+    PhoneNumber = Column(String(15), nullable=False)
+    Description = Column(Text, nullable=False)
+    LogoPath = Column(String(255))
+    Technologies = Column(Text)
+    MinGroupSize = Column(Integer, nullable=False)
+    MaxGroupSize = Column(Integer, nullable=False)
+    GroupNumber = Column(Integer, nullable=False)
+    EnglishGroup = Column(Boolean)
+    Remarks = Column(Text)
+    CooperationType = Column(Text)
+
+class ProjectReservation(Base):
+    __tablename__ = 'ProjectReservation'
+    ProjectReservationID = Column(Integer, primary_key=True)
+    ProjectID = Column(Integer, ForeignKey('Project.ProjectID'), nullable=False)
+    GroupID = Column(Integer, ForeignKey('Group.GroupID'), nullable=False)
+    IsConfirmed = Column(Boolean)
+    Status = Column(String(25), nullable=False)
+    ConfirmationPath = Column(String(255))
+
+class Group(Base):
+    __tablename__ = 'Group'
+    GroupID = Column(Integer, primary_key=True)
+    GuardianID = Column(Integer, ForeignKey('Guardian.GuardianID'))
+    Name = Column(String(50))
+    InviteCode = Column(String(10), nullable=False)
+    Size = Column(Integer)
+
+    guardian = relationship("Guardian")
+
+class User(Base):
+    __tablename__ = 'User'
+    UserID = Column(Integer, primary_key=True)
+    GroupID = Column(Integer, ForeignKey('Group.GroupID'))
+    Name = Column(String(25), nullable=False)
+    Surname = Column(String(50), nullable=False)
+    Email = Column(String(50), nullable=False)
+    Password = Column(String(255), nullable=False)
+    roleName = Column(String(25), nullable=False)
+
+class Guardian(Base):
+    __tablename__ = 'Guardian'
+    GuardianID = Column(Integer, primary_key=True)
+    Name = Column(String(25), nullable=False)
+    Surname = Column(String(50), nullable=False)
+    Email = Column(String(50), nullable=False)
+
+class ActionHistory(Base):
+    __tablename__ = 'ActionHistory'
+    HistoryID = Column(Integer, primary_key=True)
+    ReservationID = Column(Integer, ForeignKey('ProjectReservation.ProjectReservationID'), nullable=False)
+    DataTime = Column(DateTime, nullable=False)
+    Content = Column(Text, nullable=False)
+    Displayed = Column(Boolean, nullable=False)
+
+
---
+++ Backend/schemas.py
@@ -0,0 +1,118 @@
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+class ProjectBase(BaseModel):
+    CompanyName: str
+    ProjectTitle: str
+    Email: str
+    PhoneNumber: str
+    Description: str
+    LogoPath: str = None
+    Technologies: str = None
+    MinGroupSize: int
+    MaxGroupSize: int
+    GroupNumber: int
+    EnglishGroup: bool
+    Remarks: str = None
+    CooperationType: str = None
+
+class ProjectCreate(ProjectBase):
+    pass
+
+class Project(ProjectBase):
+    ProjectID: int
+
+    class Config:
+        orm_mode = True
+
+class ProjectReservationBase(BaseModel):
+    ProjectID: int
+    GroupID: int
+    IsConfirmed: Optional[bool]
+    Status: str
+    ConfirmationPath: Optional[str]
+
+class ProjectReservationCreate(ProjectReservationBase):
+    pass
+
+class ProjectReservation(ProjectReservationBase):
+    ProjectReservationID: int
+
+
+    class Config:
+        orm_mode = True
+
+"""class ProjectStatus(Enum):
+    available="AVAILABLE"
+    reserved="RESERVED"
+    taken="TAKEN"
+"""
+
+class GroupBase(BaseModel):
+    GuardianID: Optional[int]
+    Name: Optional[str]
+    InviteCode: str
+    Size: Optional[int]
+
+class GroupCreate(GroupBase):
+    pass
+
+class Group(GroupBase):
+    GroupID: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    Name: str
+    Surname: str
+    Email: str
+    Password: str
+    roleName: str
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    UserID: int
+    GroupID: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+"""class UserRole(Enum):
+    student="STUDENT"
+    reserved="RESERVED"
+    leader="LEADER"
+"""
+
+
+class GuardianBase(BaseModel):
+    Name: str
+    Surname: str
+    Email: str
+
+class GuardianCreate(GuardianBase):
+    pass
+
+class Guardian(GuardianBase):
+    GuardianID: int
+
+    class Config:
+        orm_mode = True
+
+class ActionHistoryBase(BaseModel):
+    ReservationID: int
+    DataTime: str
+    Content: str
+    Displayed: bool
+
+class ActionHistoryCreate(ActionHistoryBase):
+    pass
+
+class ActionHistory(ActionHistoryBase):
+    HistoryID: int
+
+    class Config:
+        orm_mode = True
