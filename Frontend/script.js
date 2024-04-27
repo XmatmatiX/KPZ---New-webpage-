1f58972771b092a3c39776abb1c264c1740e4daa
Klaudiaamarzec <klaudia06072002@onet.pl>
2024-04-27 23:06:06 +0200

    Frontend - admin view

--- Frontend/script.js
+++ Frontend/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
     logoutButton.addEventListener("click", function() {
         // Przenieś użytkownika do strony landingPage.html
-        window.location.href = "landingPage.html";
+        window.location.href = "../landingPage.html";
     });
 });
 
@@ -78,6 +78,94 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("groups");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "groups.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("searchStudent");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "students.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("addTopic");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "addTopic.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("addGroup");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "addGroup.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("notifications");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "notifications.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var groupsWithoutTopic = document.getElementById("settings");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    groupsWithoutTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "settings.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var topics = document.getElementById("topicsStudent");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    topics.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "topicsView.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Pobierz przycisk "Wyloguj się"
+    var newTopic = document.getElementById("addNewTopic");
+
+    // Dodaj nasłuchiwanie zdarzenia kliknięcia na przycisku "Wyloguj się"
+    newTopic.addEventListener("click", function() {
+        // Przenieś użytkownika do strony landingPage.html
+        window.location.href = "addTopic.html";
+    });
+});
+
 const login=() => {
 
     const inputLogin = document.getElementById("login");
@@ -94,4 +182,8 @@ const login=() => {
         const message = document.getElementById("errorLogin");
         message.style.display = "flex";
     }
+}
+
+const showDetails=(item) => {
+
 }
