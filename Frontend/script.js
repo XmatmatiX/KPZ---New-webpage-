654b40245dd0928b466fa891be949cac6d588f7e
Klaudia Marzec <klaudia06072002@onet.pl>
2024-05-14 23:46:43 +0200

    Landing page topics

--- Frontend/script.js
+++ Frontend/script.js
@@ -202,46 +202,114 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log("Dane")
             console.log(data)
 
-            const projects = data['projects:']; // Pobranie tablicy projektów
-            console.log("Projects")
-            console.log(projects)
+            const logos = data['logos'];
+            const companynames = data['companynames'];
+            const titles = data['titles'];
+            const projecstid = data['projecstid'];
+            const minsizes = data['minsizes'];
+            const maxsizes = data['maxsizes'];
+            const status = data['status'];
+            //const group = data['group'];
 
-            // Generowanie nowych elementów HTML na podstawie danych z tokena
-            projects.forEach(topic => {
+            function translateStatus(status) {
+                switch(status) {
+                    case 'available':
+                        return 'Dostępny';
+                    case 'reserved':
+                        return 'Zarezerwowany';
+                    case 'taken':
+                        return 'Zajęty';
+                    default:
+                        return 'Nieznany';
+                }
+            }
+
+            // Iteracja przez wszystkie projekty
+            for (let i = 0; i < titles.length; i++) {
                 const topicItem = document.createElement('div');
                 topicItem.classList.add('topicItem');
 
                 // Ustalenie tekstu dla groupSize
-                let groupMin = topic.mingroupsize;
-                let groupMax = topic.maxgroupsize;
-                if (groupMax === groupMin)
-                {
-                    topicItem.innerHTML = `
-                        <p>${topic.logopath}</p>
-                        <p>${topic.companyname}</p>
-                        <p>${topic.projecttitle}</p>
-                        <p>${topic.maxgroupsize}</p>
-                        <p>${topic.groupnumber}</p>
-                    `;
-                }
-                else {
-                    topicItem.innerHTML = `
-                        <p>${topic.logopath}</p>
-                        <p>${topic.companyname}</p>
-                        <p>${topic.projecttitle}</p>
-                        <p>${topic.mingroupsize} - ${topic.maxgroupsize}</p>
-                        <p>${topic.groupnumber}</p>
-                    `;
+                let groupSizeText = minsizes[i];
+                if (minsizes[i] !== maxsizes[i]) {
+                    groupSizeText += ` - ${maxsizes[i]}`;
                 }
 
-                // Dodanie nasłuchiwania zdarzenia kliknięcia na każdy element topicItem
-                topicItem.addEventListener('click', function() {
-                    // Wyświetlenie szczegółów tematu po kliknięciu
-                    alert(`Szczegóły tematu:\n${topic.projecttitle}\n${topic.companyname}\nGroup Size: ${topic.mingroupsize} - ${topic.maxgroupsize}\nGroup Number: ${topic.groupnumber}`);
+                // <p>${group[i] ? group[i] : 'Brak'}</p>
+                const translatedStatus = translateStatus(status[i]);
+
+                // Utworzenie HTML dla pojedynczego projektu
+                topicItem.innerHTML = `
+                    <p>${logos[i] ? logos[i] : 'Brak'}</p>
+                    <p>${companynames[i]}</p>
+                    <p>${titles[i]}</p>
+                    <p>${groupSizeText}</p>
+                    <p>${translatedStatus}</p>
+                `;
+
+                // Dodanie nasłuchiwania zdarzenia kliknięcia na każdy element topicItemAdmin
+                topicItem.addEventListener('click', function () {
+                    // Przekierowanie użytkownika do widoku reservationDetails, przekazując ID projektu jako parametr w adresie URL
+                    window.location.href = `topicDetails.html?id=${projecstid[i]}`;
                 });
 
+                // Dodanie pojedynczego projektu do listy
                 topicList.appendChild(topicItem);
-            });
+            }
         })
         .catch(error => console.error('Błąd pobierania danych:', error));
 });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Pobranie danych z endpointa GET /ProjectList
+//     fetch('http://127.0.0.1:8000/ProjectList')
+//         .then(response => response.json())
+//         .then(data => {
+//             const topicList = document.getElementById('topicList');
+//
+//             console.log("Dane")
+//             console.log(data)
+//
+//             const projects = data['projects:']; // Pobranie tablicy projektów
+//             console.log("Projects")
+//             console.log(projects)
+//
+//             // Generowanie nowych elementów HTML na podstawie danych z tokena
+//             projects.forEach(topic => {
+//                 const topicItem = document.createElement('div');
+//                 topicItem.classList.add('topicItem');
+//
+//                 // Ustalenie tekstu dla groupSize
+//                 let groupMin = topic.mingroupsize;
+//                 let groupMax = topic.maxgroupsize;
+//                 if (groupMax === groupMin)
+//                 {
+//                     topicItem.innerHTML = `
+//                         <p>${topic.logopath}</p>
+//                         <p>${topic.companyname}</p>
+//                         <p>${topic.projecttitle}</p>
+//                         <p>${topic.maxgroupsize}</p>
+//                         <p>${topic.groupnumber}</p>
+//                     `;
+//                 }
+//                 else {
+//                     topicItem.innerHTML = `
+//                         <p>${topic.logopath}</p>
+//                         <p>${topic.companyname}</p>
+//                         <p>${topic.projecttitle}</p>
+//                         <p>${topic.mingroupsize} - ${topic.maxgroupsize}</p>
+//                         <p>${topic.groupnumber}</p>
+//                     `;
+//                 }
+//
+//                 // Dodanie nasłuchiwania zdarzenia kliknięcia na każdy element topicItem
+//                 topicItem.addEventListener('click', function() {
+//                     // Wyświetlenie szczegółów tematu po kliknięciu
+//                     alert(`Szczegóły tematu:\n${topic.projecttitle}\n${topic.companyname}\nGroup Size: ${topic.mingroupsize} - ${topic.maxgroupsize}\nGroup Number: ${topic.groupnumber}`);
+//                 });
+//
+//                 topicList.appendChild(topicItem);
+//             });
+//         })
+//         .catch(error => console.error('Błąd pobierania danych:', error));
+// });
---
+++ Frontend/topicDetails.js
@@ -0,0 +1,65 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Odczytanie ID projektu z adresu URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const projectId = urlParams.get('id');
+    console.log("ID: ", projectId)
+
+    // Pobranie szczegółów projektu za pomocą endpointu /Project/{id}
+    fetch(`http://127.0.0.1:8000/Project/${projectId}`)
+        .then(response => response.json())
+        .then(projectData => {
+
+            console.log("Project")
+            console.log(projectData)
+
+            const projectDetailsElement = document.getElementById('topicHeader');
+            projectDetailsElement.innerHTML = `
+                <img class="logo" src="Images/logo.png" alt="There should be a logo here">
+                <p class="companyName">${projectData.companyname}</p>
+            `;
+
+            //<p>${projectData.logopath}</p>
+
+            const minGroupSize = projectData.mingroupsize;
+            const maxGroupSize = projectData.maxgroupsize;
+            const englishValue = projectData.englishgroup;
+
+            const language = englishValue ? "Tak" : "Nie";
+
+            // Sprawdź, czy minGroupSize i maxGroupSize są takie same
+            const groupSizeText = minGroupSize === maxGroupSize ? minGroupSize : `${minGroupSize} - ${maxGroupSize}`;
+
+            const topicDetails = document.getElementById('topicDetails')
+            topicDetails.innerHTML = `
+                <p class="topicName">${projectData.projecttitle}</p>
+                <p class="topicDescription">${projectData.description}</p>
+                 ${projectData.cooperationtype ? `
+                    <p class="topicLabel">Planowane formy współpracy:</p>
+                    <p class="details">${projectData.cooperationtype}</p>` : ''}
+                <div class="someDetails">
+                    <p class="topicLabel2">Akceptowana wielkość grup:</p>
+                    <p class="details2">${groupSizeText}</p>
+                </div>
+                
+                ${englishValue !== null ? `
+                <div class="someDetails">
+                    <p class="topicLabel2">Język angielski jako dopuszczalny język:</p>
+                    <p class="details2">${language}</p>
+                </div>` : ''}
+                <div class="someDetails">
+                    <p class="topicLabel2">Liczba grup:</p>
+                    <p class="details2">${projectData.groupnumber}</p>
+                </div>
+                ${projectData.technologies ? `
+                    <p class="topicLabel">Technologie:</p>
+                    <p class="details">${projectData.technologies}</p>` : ''}
+                ${projectData.remarks ? `
+                    <p class="topicLabel">Dodatkowe uwagi:</p>
+                    <p class="details">${projectData.remarks}</p>` : ''}
+            `;
+
+        })
+        .catch(error => console.error('Błąd pobierania danych projektu:', error));
+});
