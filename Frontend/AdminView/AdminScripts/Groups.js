78d7caa630f9549050efd5579c8edd0a9526ed33
Mateusz Boroch <m.boroch2@gmail.com>
2024-06-05 18:49:26 +0200

    fix admin gorups.js

--- Frontend/AdminView/AdminScripts/Groups.js
+++ Frontend/AdminView/AdminScripts/Groups.js
@@ -1,8 +1,8 @@
 "use strict"
 
+const token = sessionStorage.getItem("JWT");
 document.addEventListener("DOMContentLoaded", function() {
     // Pobranie danych z endpointa GET /ProjectList
-    const token = sessionStorage.getItem("JWT");
     fetch('http://127.0.0.1:8000/Admin/Groups',{
         headers: {
                     "Authorization": `Bearer ${token}`
@@ -18,52 +18,31 @@ document.addEventListener("DOMContentLoaded", function() {
             const leaders = groups['leaders'];
             const groupsize = groups['groupsize'];
             const guardians = groups['guardians'];
-            const projects = groups['projects'];
+            const projectTitles = groups['project_titles'];
+            const companys = groups['companys'];
 
-            // Tworzenie tablicy obietnic fetch
             const fetchGuardianPromises  = guardians.map(guardianId => {
-                return fetch(`http://127.0.0.1:8000/Admin/Guardian/${guardianId}`, {
-                    headers: {
-                    "Authorization": `Bearer ${token}`
-                 }
-                })
-                    .then(response => response.json())
-                    .then(data => `${data.name} ${data.surname}`)
-                    .catch(error => {
-                        console.error('Błąd pobierania danych:', error);
-                        return ''; // Zwrócenie pustego ciągu w przypadku błędu
-                    });
-            });
+                if(guardianId) {
+                    return fetch(`http://127.0.0.1:8000/Admin/Guardian/${guardianId}`,{
+                        headers: {
+                            "Authorization": `Bearer ${token}`
+                        }
+                        })
+                        .then(response => response.json())
+                        .then(data => `${data.name} ${data.surname}`)
+                        .catch(error => {
+                            console.error('Błąd pobierania danych:', error);
+                            return '';
+                        });
+                }
+                else {
+                    return 'Brak';
+                }
 
-            const fetchProjectPromises = projects.map(project => {
-                return fetch(`http://127.0.0.1:8000/Admin/Project/${project.projectid}`,{
-                    headers: {
-                    "Authorization": `Bearer ${token}`
-                 }
-                })
-                    .then(response => {
-                        if(response.ok)
-                        {
-                            return response.json();
-                        }
-                        else {
-                            return {};
-                        }
-                    })
-                    .then(data => {
-                        return {
-                            companyName: data.companyname,
-                            projectTitle: data.projecttitle
-                        };
-                    })
-                    .catch(error => {
-                        console.error('Błąd pobierania danych projektu:', error);
-                        return {}; // Zwrócenie pustego obiektu w przypadku błędu
-                    });
             });
 
             // Oczekiwanie na zakończenie wszystkich żądań fetch
-            Promise.all([...fetchGuardianPromises, ...fetchProjectPromises])
+            Promise.all([...fetchGuardianPromises])
                 .then(dataArray  => {
 
                     const guardiansData = dataArray.slice(0, guardians.length);
@@ -74,18 +53,16 @@ document.addEventListener("DOMContentLoaded", function() {
                         groupItem.classList.add('groupItem');
 
                         const guardian = guardiansData[i];
-                        const project = projectsData[i];
 
                         groupItem.innerHTML = `
                             <p>${groupids[i]}</p>
-                            <p>${project.companyName}</p>
-                            <p>${project.projectTitle}</p>
+                            <p>${companys[i]}</p>
+                            <p>${projectTitles[i]}</p>
                             <p>${groupsize[i]}</p>
                             <p>${guardian}</p>
                         `;
 
                         groupItem.addEventListener('click', function() {
-                            // Przekierowanie użytkownika do widoku reservationDetails, przekazując ID projektu jako parametr w adresie URL
                             window.location.href = `groupDetails.html?id=${groupids[i]}`;
                         });
 
